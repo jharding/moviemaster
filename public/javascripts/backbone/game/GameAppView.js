6dad68c8d441e8eb31cb935087e3939cee64ac59
Jake Harding <jacob.s.harding@gmail.com>
2011-08-27 13:04:02 -0700

    Now listening for updates to the game. set up for testing right now.

--- public/javascripts/backbone/game/GameAppView.js
+++ public/javascripts/backbone/game/GameAppView.js
@@ -11,12 +11,13 @@ var GameAppView = Backbone.View.extend({
 
     this.delegateEvents(this.events);
 
-    _.bindAll(this, 'render', 'submitAnswer', 'startGame');
+    _.bindAll(this, 'render', 'submitAnswer', 'startGame', 'updateGame');
 
     this.users = new Users();
     this.clips = new Clips();
 
     channel.bind('startGame', this.startGame, this);
+    channel.bind('answerEvent', this.updateGame, this);
   },
 
   render: function() {
@@ -39,5 +40,22 @@ var GameAppView = Backbone.View.extend({
     this.gameStarted = true;
     this.clips.view.nextClip();
     videoLoop = setInterval('App.clips.view.nextClip()', 60000); 
+  },
+
+  updateGame: function(data) {
+    alert(JSON.stringify(data));
+    /* 
+    var questionType = data.question;
+    var result = data.result;
+    this.userToUpdate = data.userId;
+
+    var user = this.users.find(function(user) {
+      return user.get('_id') === this.userToUpdate;
+    }, this);
+
+    if (result === 'right') {
+      user.set('points', user.get('points') + 1);
+    }
+    */
   }
 });
