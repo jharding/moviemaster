1f5a0b6137c7082dcdc234a67066ccecfeff3293
Jake Harding <jacob.s.harding@gmail.com>
2011-08-28 12:50:06 -0700

    Leaderboard is finished.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -13,4 +13,18 @@ $(document).ready(function() {
   $('#share-form').submit(function(event) {
     event.preventDefault();
   });
+
+  $('#leaderboards #tabs .tab').click(function() {
+    $('#leaderboards #tabs .tab').removeClass('selected');
+    $(this).addClass('selected');
+    if ($(this).attr('id') === 'wins-tab') {
+      $('#leaderboards #wins-list').show();
+      $('#leaderboards #points-list').hide();
+    }
+
+    else {
+      $('#leaderboards #wins-list').hide();
+      $('#leaderboards #points-list').show();
+    }
+  });
 });
