54ee6525846767b9db1eb6326481fefa73eb22e3
Jake Harding <jacob.s.harding@gmail.com>
2011-08-27 23:22:44 -0700

    Server wasn't sending back the results of an attempted answer

--- app.js
+++ app.js
@@ -350,8 +350,12 @@ app.post('/game/:id/answer', verifyUser, function(req, res) {
 			gameAnswerChannel.trigger("answerEvent", {userId: req.user._id, question: req.body.question, answer: answer, result: result});
 			if (result == 'right') {
 				User.update({_id: req.user._id}, {$inc: {points: 1}}, {multi: false}, function(err, doc){});
+        res.send({ match: true });
 			}
-			res.send();
+
+      else {
+        res.send({ match: false });
+      }
 		});
 		
 	}
